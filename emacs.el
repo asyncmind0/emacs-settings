7084704c77347aeb0b7e4b5682711ae5ae6f2aa1
Steven Joseph <stevenjose@gmail.com>
2012-05-18 16:58:14 +1000

    updated emacs.el

--- emacs.el
+++ emacs.el
@@ -192,3 +192,30 @@
 (defun edit-emacs-config ()
   (interactive)
   (find-file "~/.emacs.d/emacs.el"))
+
+
+;; ediff cleanup 
+(defvar my-ediff-bwin-config nil "Window configuration before ediff.")
+(defcustom my-ediff-bwin-reg ?b
+  "*Register to be set up to hold `my-ediff-bwin-config'
+    configuration.")
+
+(defun my-ediff-bsh ()
+  "Function to be called before any buffers or window setup for
+    ediff."
+  (remove-hook 'ediff-quit-hook 'ediff-cleanup-mess)
+  (window-configuration-to-register my-ediff-bwin-reg))
+
+(defun my-ediff-aswh ()
+"setup hook used to remove the `ediff-cleanup-mess' function.  It causes errors."
+  (remove-hook 'ediff-quit-hook 'ediff-cleanup-mess))
+
+(defun my-ediff-qh ()
+  "Function to be called when ediff quits."
+  (remove-hook 'ediff-quit-hook 'ediff-cleanup-mess)
+  (ediff-cleanup-mess)
+  (jump-to-register my-ediff-bwin-reg))
+
+(add-hook 'ediff-before-setup-hook 'my-ediff-bsh)
+(add-hook 'ediff-after-setup-windows-hook 'my-ediff-aswh);
+(add-hook 'ediff-quit-hook 'my-ediff-qh)
