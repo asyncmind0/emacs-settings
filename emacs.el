ebba944093ee73dd2375bf79621c002aab1d7070
Steven Joseph <stevenjose@gmail.com>
2012-05-21 17:13:26 +1000

    finally working cscope almost vim now

--- emacs.el
+++ emacs.el
@@ -12,11 +12,19 @@
 (setq paredit-mode 0)
 (require 'package)
 (package-initialize)
+(require 'xclip)
+(require 'uniquify)
+(setq uniqueify-buffer-name-style 'reverse)
+
+;; CScope =============================================================================
+(require 'xcscope)
 ;; Evil =============================================================================
 (require 'evil)
+(require 'evil-search)
 ;;http://dnquark.com/blog/2012/02/emacs-evil-ecumenicalism/
 (evil-mode 1)
 (setq evil-default-state 'normal)
+(setq evil-flash-delay 60)
 (define-key global-map [f3] 'buffer-menu)
 (define-key evil-insert-state-map (kbd "C-w") 'evil-window-map)
 (define-key evil-insert-state-map (kbd "C-w <left>") 'evil-window-left)
@@ -24,6 +32,9 @@
 (define-key evil-insert-state-map (kbd "C-w <up>") 'evil-window-up)
 (define-key evil-insert-state-map (kbd "C-w <down>") 'evil-window-down)
 (define-key evil-normal-state-map (kbd "`") 'find-file)
+(define-key evil-normal-state-map (kbd "C-]") 'cscope-find-global-definition-no-prompting)
+(evil-define-key 'normal evil-normal-state-map "C-c d" 'cscope-find-symbol)
+(define-key evil-normal-state-map (kbd "C-t") 'cscope-pop-mark)
 ;;(define-key global-map (kbd "`") 'find-file)
 (evil-define-command "Ve" (function 
                             lambda() (split-window-horizontally)))
@@ -82,6 +93,7 @@
     (local-set-key (kbd "C-c <left>")  'hs-hide-block)
     (local-set-key (kbd "C-c <up>")    'hs-hide-all)
     (local-set-key (kbd "C-c <down>")  'hs-show-all)
+    (local-set-key (kbd "C-]")  'cscope-find-symbol)
     ;;(hs-minor-mode t)
     ;;(hs-hide-all)
     ;;(setq autopair-handle-action-fns
@@ -100,15 +112,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ack-and-a-half-prompt-for-directory t)
+ '(compilation-disable-input t)
+ '(cscope-do-not-update-database t)
  '(custom-enabled-themes (quote (tango-2-steven)))
- '(custom-safe-themes (quote ("7c66e61cada84d119feb99a90d30da44fddc60f386fca041c01de74ebdd934c2" "f41ff26357e8ad4d740901057c0e2caa68b21ecfc639cbc865fdd8a1cb7563a9" "1797bbff3860a9eca27b92017b96a0df151ddf2eb5f73e22e37eb59f0892115e" "21d9280256d9d3cf79cbcf62c3e7f3f243209e6251b215aede5026e0c5ad853f" default)))
+ '(custom-safe-themes (quote ("3800c684fc72cd982e3366a7c92bb4f3975afb9405371c7cfcbeb0bee45ddd18" "7c66e61cada84d119feb99a90d30da44fddc60f386fca041c01de74ebdd934c2" "f41ff26357e8ad4d740901057c0e2caa68b21ecfc639cbc865fdd8a1cb7563a9" "1797bbff3860a9eca27b92017b96a0df151ddf2eb5f73e22e37eb59f0892115e" "21d9280256d9d3cf79cbcf62c3e7f3f243209e6251b215aede5026e0c5ad853f" default)))
  '(ecb-options-version "2.40")
  '(ecb-source-path (quote ("/home/steven/iress/xplan/")))
  '(ediff-split-window-function (quote split-window-horizontally))
- '(compilation-disable-input t)
  '(grep-command "ack --with-filename --nogroup --all")
- '(ack-and-a-half-prompt-for-directory t)
- '(paredit-mode 0)
+ '(lazy-highlight-cleanup nil)
+ '(lazy-highlight-initial-delay 0)
+ '(lazy-highlight-max-at-a-time nil)
+ '(paredit-mode nil t)
  '(recentf-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -125,8 +141,7 @@
  
 ;; python ropemacs and pyemacs
 ;;https://github.com/mzc/ropemacs
-(require 'xclip)
-;;(require 'xcscope)
+
 (add-to-list 'load-path "~/.emacs.d/")
 (autoload 'ack-and-a-half-same "ack-and-a-half" nil t)
 (autoload 'ack-and-a-half "ack-and-a-half" nil t)
